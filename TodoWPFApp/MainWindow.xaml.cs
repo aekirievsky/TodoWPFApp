d95ec45829bdbaf5524eb7e785b36567d4fd77e3
aekirievsky <aekirievsky@gmail.com>
2024-02-19 23:44:32 +0500

    Добавил разметку левой части приложения / Стили для кнопок

--- TodoWPFApp/MainWindow.xaml.cs
+++ TodoWPFApp/MainWindow.xaml.cs
@@ -1,24 +1,25 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TodoWPFApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
         }
     }
 }
