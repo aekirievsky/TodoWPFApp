546fd05d6bd76106756def4ddf57b5aad0bc81e7
aekirievsky <aekirievsky@gmail.com>
2024-02-20 22:14:58 +0500

    Добавил поле для добавления заметок / Стили для поля заметок / User Control Item

--- TodoWPFApp/MainWindow.xaml.cs
+++ TodoWPFApp/MainWindow.xaml.cs
@@ -21,5 +21,39 @@ namespace TodoWPFApp
             }
 
         }
+
+        private void lblNote_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtNote.Focus();
+        }
+
+        private void txtNote_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNote.Text) && txtNote.Text.Length > 0)
+            {
+                lblNote.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lblNote.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void lblTime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtTime.Focus();
+        }
+
+        private void txtTime_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtTime.Text) && txtTime.Text.Length > 0)
+            {
+                lblTime.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lblTime.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
